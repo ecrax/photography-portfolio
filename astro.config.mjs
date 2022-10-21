342adbc873ffd34446efa112f3fd8e0678bf89df
Leo Kling <leo.kling.info@gmail.com>
2022-10-21 21:08:22 +0200

    i hate images

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [image(
+    {
+      serviceEntryPoint: '@astrojs/image/sharp'
+    }
+  ), tailwind()],
+});
